fa9eb4d67fb0d7056de9bdaf1cca58ea1010784b
ernestlv <ernestolv@hotmail.com>
2023-05-25 18:18:13 -0400

    add util.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './styles.css';
+import { print, first, last, middle, replace, pairs, max, min } from "./util";
+
+print("Hello", "World", "!!!");
+var arr = [0,1,2,3];
+print(middle(arr));
+print(replace(1, 5, arr));
+print(pairs({a:1, b:2, c:3}))
+print(max(arr));
+print(min(arr));
---
+++ src/util.js
@@ -0,0 +1,52 @@
+/**
+  *
+  * Handles immutable arrays
+  *
+**/
+export const print = function (...msg) {
+  const timestamp = new Date();
+  console.log(`${timestamp}: ${msg.join(', ')}`);
+}
+
+export const first = function (arr) {
+  const [first] = arr;
+  return first;
+}
+
+export const last = function (arr) {
+  const [last] = arr.reverse();
+  return last;
+}
+
+export const middle = function (arr) {
+  const [first, ...remain] = arr;
+  const [last, ...middle] = remain.reverse();
+  return middle.reverse();
+}
+
+export const replace = function (orgElement, newElement, arr) {
+  return arr.map(element => element === orgElement ? newElement : element);
+}
+
+export const max = function (arr, init=0) {
+  return arr.reduce((max, element) => element > max ? element : max, init);
+}
+
+export const min = function (arr, init=Infinity) {
+  return arr.reduce((min, element) => element < min ? element : min, init);
+}
+
+export const pairs = function (obj) {
+  return Object.keys(obj).map(key => ({key, value:obj[key]}))
+}
+
+export const objectify = function(arr) {
+  return arr.reduce(function (obj, {key, value}) {
+     obj[key] = value;
+     return obj;
+  }, {});
+}
+
+export const unique = function(arr) {
+  return arr.reduce((unique, element) => unique.indexOf(element) === -1 ? [...unique, element] : unique, []);
+}
